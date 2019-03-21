b40072ef398ae0c339894302f46cd91307f89b03
Alex Dekau <alexander.j.dekau@wmich.edu>
2019-03-21 17:52:00 -0400

    remove trailing white space

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <small class="created-by">
-      <i class="nb-angle-double-right"></i> Created by <strong>Pacas (potatofarms.9241)</strong>. 
+      <i class="nb-angle-double-right"></i> Created by <strong>Pacas (potatofarms.9241)</strong>.&nbsp;
       Data and assets provided by <strong><a href="https://guildwars2.com" target="_blank">Guild Wars 2</a></strong>
     </small>
     <div class="socials">
