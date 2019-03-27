c92d7067ddc275095a17780e75f47700919add1c
Alex Dekau <alexander.j.dekau@wmich.edu>
2019-03-27 00:15:40 -0400

    make names consistent

--- src/app/@core/models/skirmish.model.ts
+++ src/app/@core/models/skirmish.model.ts
@@ -1,8 +1,8 @@
 import { MatchScores } from './matchscores.model';
-import { MapScores } from './maps.model';
+import { IMapScores } from './maps.model';
 
 export interface Skirmish {
     id: number;
     scores: MatchScores;
-    map_scores: MapScores[];
+    map_scores: IMapScores[];
 }
