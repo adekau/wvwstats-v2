f96f632edc3d7b7a30ed9574b79f5c8a9b7abaee
Alex Dekau <alexander.j.dekau@wmich.edu>
2019-03-27 00:14:41 -0400

    create class for map data

--- src/app/@core/models/maps.model.ts
+++ src/app/@core/models/maps.model.ts
@@ -1,20 +1,75 @@
 import { MatchScores } from './matchscores.model';
 import { MapBonus } from './mapbonus.model';
-import { Objective } from './objective.model';
+import { IObjective, IObjectiveCount } from './objective.model';
 import { MatchDeaths } from './matchdeaths.model';
 import { MatchKills } from './matchkills.model';
+import { IMatchPPT } from './matchppt.model';
 
-export interface MapScores {
-    type: string;
-    scores: MatchScores;
+export interface IMapScores {
+  type: string;
+  scores: MatchScores;
 }
 
-export interface Map {
-    id: number;
-    type: string;
-    scores: MatchScores;
-    bonuses?: MapBonus[];
-    objectives: Objective[];
-    deaths?: MatchDeaths;
-    kills?: MatchKills;
+export interface IMap {
+  id: number;
+  type: string;
+  scores: MatchScores;
+  objectives: IObjective[];
+  deaths: MatchDeaths;
+  kills: MatchKills;
+  bonuses?: MapBonus[];
+}
+
+export class Map implements IMap {
+  id: number;
+  type: string;
+  scores: MatchScores;
+  objectives: IObjective[];
+  deaths: MatchDeaths;
+  kills: MatchKills;
+  bonuses?: MapBonus[];
+
+  constructor(map: IMap) {
+    this.id = map.id;
+    this.type = map.type;
+    this.scores = map.scores;
+    this.objectives = map.objectives;
+    this.deaths = map.deaths;
+    this.kills = map.kills;
+
+    if (map.bonuses) {
+      this.bonuses = map.bonuses;
+    }
+  }
+
+  get ppt() {
+    const ppt: IMatchPPT = { red: 0, blue: 0, green: 0 };
+    this.objectives.forEach(objective => {
+      if (!objective.owner || !objective.points_tick) {
+        return;
+      }
+
+      ppt[objective.owner.toLowerCase()] += objective.points_tick;
+    });
+    return ppt;
+  }
+
+  get objectiveCount(): IObjectiveCount {
+    let oc: IObjectiveCount = {
+      camps: { red: 0, blue: 0, green: 0 },
+      towers: { red: 0, blue: 0, green: 0 },
+      keeps: { red: 0, blue: 0, green: 0 },
+      castles: { red: 0, blue: 0, green: 0 },
+    };
+
+    this.objectives.forEach(objective => {
+      if (!objective.owner || !objective.points_tick) {
+        return;
+      }
+
+      // oc[objective.]
+    });
+
+    return oc;
+  }
 }
