4b44a5f56f8b60bcdbe83fd1764fe9db3cf0458e
Alex Dekau <alexander.j.dekau@wmich.edu>
2019-03-27 21:52:30 -0400

    helper functions for objective collections like the others

--- src/app/@core/collections/objective.collection.ts
+++ src/app/@core/collections/objective.collection.ts
@@ -1,9 +1,25 @@
 import { IObjective, Objective } from '../models/objective.model';
 
 export class ObjectiveCollection {
-  objectives: Array<IObjective>;
+  objectives: Array<Objective>;
 
   constructor(response: IObjective[] = []) {
     this.objectives = response.map(obj => new Objective(obj));
+  }
+
+  find(id: string): Objective {
+    return this.objectives.find(objective => objective.id === id);
+  }
+
+  all(): Array<Objective> {
+    return this.objectives;
+  }
+
+  filterTypes(...types: string[]): ObjectiveCollection {
+    return new ObjectiveCollection(
+      this.objectives
+        .filter(objective => objective.type)
+        .filter(objective => types.includes(objective.type))
+    );
   }
 }
