f0cc19c11287624dfe4cba390c6c99bec71bf651
Alex Dekau <alexander.j.dekau@wmich.edu>
2019-03-27 21:52:17 -0400

    remove unnecessary modules

--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -1,7 +1,5 @@
 import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NbSecurityModule, NbRoleProvider } from '@nebular/security';
-import { of as observableOf } from 'rxjs';
 
 import { throwIfAlreadyLoaded } from './module-import-guard';
 import {
@@ -18,34 +16,9 @@ const DATA_SERVICES = [
   // example { provide: UserData, useClass: UserService },
 ];
 
-export class NbSimpleRoleProvider extends NbRoleProvider {
-  getRole() {
-    // here you could provide any role based on any auth flow
-    return observableOf('guest');
-  }
-}
-
 export const NB_CORE_PROVIDERS = [
   ...ServicesModule.forRoot().providers,
   ...DATA_SERVICES,
-
-  NbSecurityModule.forRoot({
-    accessControl: {
-      guest: {
-        view: '*',
-      },
-      user: {
-        parent: 'guest',
-        create: '*',
-        edit: '*',
-        remove: '*',
-      },
-    },
-  }).providers,
-
-  {
-    provide: NbRoleProvider, useClass: NbSimpleRoleProvider,
-  },
   AnalyticsService,
   LayoutService,
   StateService,
