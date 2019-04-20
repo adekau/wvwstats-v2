5929bf899f0cab8ba37cd4eedeaba5e0b82a4938
Alexander J Dekau <alexander.j.dekau@wmich.edu>
2019-04-20 02:05:15 -0400

    update menu

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -2,14 +2,53 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'NA Matches',
-    icon: 'wvwstats-icon-na',
-    link: '/na',
-    home: true,
+    title: 'WVWSTATS',
+    group: true,
   },
   {
-    title: 'EU Matches',
-    icon: 'wvwstats-icon-eu',
-    link: '/eu',
+    title: 'Matches',
+    icon: 'nb-home',
+    expanded: true,
+    children: [
+      {
+        title: 'NA Matches',
+        icon: 'wvwstats-icon-na',
+        link: '/na',
+        home: true,
+      },
+      {
+        title: 'EU Matches',
+        icon: 'wvwstats-icon-eu',
+        link: '/eu',
+      },
+    ],
+  },
+  {
+    title: 'Game Data',
+    icon: 'nb-help',
+    children: [
+      {
+        title: 'Map',
+        icon: 'nb-location',
+      },
+    ],
+  },
+  {
+    title: 'Advanced',
+    icon: 'nb-gear',
+    children: [
+      {
+        title: 'Leaderboard',
+        icon: 'nb-list',
+      },
+      {
+        title: 'Timezones',
+        icon: 'nb-sunny',
+      },
+      {
+        title: 'Grapher',
+        icon: 'nb-bar-chart',
+      },
+    ],
   },
 ];
