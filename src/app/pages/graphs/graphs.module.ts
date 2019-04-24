70f78a215740d165adad1e126919984fa787ed49
Alex Dekau <alexander.j.dekau@wmich.edu>
2019-04-24 10:58:43 -0400

    scaffold graphs page

---
+++ src/app/pages/graphs/graphs.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GraphsComponent } from './graphs.component';
+
+@NgModule({
+  declarations: [GraphsComponent],
+  imports: [
+    CommonModule,
+  ],
+  entryComponents: [GraphsComponent],
+})
+export class GraphsModule { }
