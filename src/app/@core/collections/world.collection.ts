20a37b90c4cbd75759ff874325551c727c30cf8a
Alex Dekau <alexander.j.dekau@wmich.edu>
2019-04-24 10:56:09 -0400

    helper function for sorting and filtering by region

--- src/app/@core/collections/world.collection.ts
+++ src/app/@core/collections/world.collection.ts
@@ -1,4 +1,5 @@
 import { IWorld, World } from '../models/world.model';
+import { GW2Region } from '../enums/gw2region.enum';
 
 export class WorldCollection {
   worlds: Array<World> = [];
@@ -21,6 +22,38 @@ export class WorldCollection {
 
   all() {
     return this.worlds;
+  }
+
+  region(region: GW2Region): WorldCollection {
+    return new WorldCollection(
+      this.worlds.filter(world => world.id.toString().startsWith(region.toString(10))),
+    );
+  }
+
+  sortBy(col: 'id' | 'name', order: 'asc' | 'desc'): WorldCollection {
+    return new WorldCollection(
+      this.worlds.sort((a: World, b: World) => {
+        if (col === 'id') {
+          if (order === 'desc') {
+            return b.id - a.id;
+          } else if (order === 'asc') {
+            return a.id - b.id;
+          } else {
+            throw Error('Unknown sort order.');
+          }
+        } else if (col === 'name') {
+          if (order === 'desc') {
+            return b.name > a.name ? 1 : b.name === a.name ? 0 : -1;
+          } else if (order === 'asc') {
+            return a.name > b.name ? 1 : a.name === b.name ? 0 : -1;
+          } else {
+            throw Error('Unknown sort order.');
+          }
+        } else {
+          throw Error('Unknown column to sort by.');
+        }
+      }),
+    );
   }
 
   subset(...ids: number[]): WorldCollection {
